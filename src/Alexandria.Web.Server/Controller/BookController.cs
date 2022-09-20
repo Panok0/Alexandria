4228b4ebacaf4a33c586fbbe79727494faea3941
Panagiotis Banagos <p.banagos@yahoo.gr>
2022-09-20 21:55:41 +0300

    Adds basic operations for books

--- src/Alexandria.Web.Server/Controller/BookController.cs
+++ src/Alexandria.Web.Server/Controller/BookController.cs
@@ -1,5 +1,7 @@
 namespace Mpanagos.Alexandria.Web.Server.Controller;
 
+using System.Runtime.InteropServices;
+
 using Microsoft.AspNetCore.Mvc;
 
 using Mpanagos.Alexandria.Data;
@@ -19,5 +21,55 @@ public class BookController
   public List<Book> GetBooks()
   {
     return ctx.Books.ToList();
+  }
+
+  [HttpGet("{id}")]
+  public Book GetBook(long id)
+  {
+    return ctx.Books.FirstOrDefault(x => x.Id == id);
+  }
+
+  [HttpPost("")]
+  public Book CreateBook(Book book)
+  {
+    var b = new Book
+    {
+      Description = book.Description,
+      Title = book.Title,
+    };
+    ctx.Books.Add(b);
+    ctx.SaveChanges();
+
+    return b;
+  }
+
+  [HttpPut("{id}")]
+  public Book UpdateBook(long id, Book book)
+  {
+    var b = ctx.Books.FirstOrDefault(x => x.Id == id);
+    if (b == null)
+    {
+      return null;
+    }
+
+    b.Description = book.Description;
+    b.Title = book.Title;
+
+    ctx.SaveChanges();
+
+    return b;
+  }
+
+  [HttpDelete("{id}")]
+  public void DeleteBook(long id)
+  {
+    var b = ctx.Books.FirstOrDefault(x => x.Id == id);
+    if (b == null)
+    {
+      return;
+    }
+
+    ctx.Books.Remove(b);
+    ctx.SaveChanges();
   }
 }
